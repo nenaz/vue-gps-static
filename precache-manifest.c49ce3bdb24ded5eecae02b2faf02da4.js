ccad0f753a01d94fd22a1f9cac8e3fab3c09d34a
MrNenaz <mr444@mail.ru>
2021-08-29 00:49:42 +0300

    add first commit

---
+++ precache-manifest.c49ce3bdb24ded5eecae02b2faf02da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f43920da01b79bbd67",
+    "url": "/css/app.59f088a1.css"
+  },
+  {
+    "revision": "2d4bd5cefd759c681b01",
+    "url": "/css/chunk-vendors.b5a8e3f3.css"
+  },
+  {
+    "revision": "b098dfd551a6c47f8cc19c7a30d5f320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f43920da01b79bbd67",
+    "url": "/js/app.d54c37ce.js"
+  },
+  {
+    "revision": "2d4bd5cefd759c681b01",
+    "url": "/js/chunk-vendors.740fed1c.js"
+  },
+  {
+    "revision": "702280001fb9bbab192c53a9fa2d7eff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
